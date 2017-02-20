688272b4ddd0d60732792f9c62422b01a1cb281b
nperti <pertierra.nicolas@gmail.com>
2017-02-19 23:35:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/nico/abrirpagina/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nico.abrirpagina;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startActivity(View intent) {
+        Uri webpage = Uri.parse("http://www.android.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(webIntent);
+    }
+
+
+
+
+
+}
